d21a0aba06dda0afcd09880399540369e22e87de
Claire Filipek <cfilipek@sva.edu>
2020-10-07 13:03:59 -0400

    changing background added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+const images = [
+  'https://i.imgur.com/tKMCb1c.jpg',
+  'https://i.imgur.com/Yrp5SQQ.jpg',
+  'https://i.imgur.com/Qi59Hp9.jpg',
+];
+var nextimage = 0;
+doSlideshow();
+
+function doSlideshow() {
+  if (nextimage >= images.length) {
+    nextimage = 0;
+  }
+  $('.head__body')
+    .css('background-image', 'url("' + images[nextimage++] + '")')
+    .fadeIn(200, function() {
+      setTimeout(doSlideshow, 3000);
+    });
+}
