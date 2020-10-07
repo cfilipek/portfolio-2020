13124d5d9dfbd372d26b491d40d63689c51ad164
Claire Filipek <cfilipek@sva.edu>
2020-10-07 18:38:23 -0400

    added loading screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './sass/main.scss';
 
+let lastScrollY = 0,
+  delayFlag = true,
+  delayTime = 4000;
+
+$(function() {
+  $('body').addClass('noscroll');
+
+  window.setTimeout(function() {
+    $('body').removeClass('noscroll');
+  }, delayTime);
+});
+
+// preloader
+const preloaderTL = gsap.timeline();
+preloaderTL.to('#logo', { yPercent: -20, opacity: 0, delay: 4 });
+preloaderTL.to('.preloader', {
+  transform: 'scaleY(0)',
+  transformOrigin: 'top',
+  delay: '-=3',
+});
+
 const images = [
   'https://i.imgur.com/tKMCb1c.jpg',
   'https://i.imgur.com/Yrp5SQQ.jpg',
